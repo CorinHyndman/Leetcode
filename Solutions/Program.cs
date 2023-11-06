f106feae800b0bbb936ae8677b7fa2fa13223a22
Corin <corinhyndman@protonmail.com>
2023-11-06 12:53:34 +0000

    Static cleanup

--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -7,17 +7,6 @@ using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
-
-SeatManager seatManager = new SeatManager(5); // Initializes a SeatManager with 5 seats.
-seatManager.Reserve();    // All seats are available, so return the lowest numbered seat, which is 1.
-seatManager.Reserve();    // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
-seatManager.Unreserve(2); // Unreserve seat 2, so now the available seats are [2,3,4,5].
-seatManager.Reserve();    // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
-seatManager.Reserve();    // The available seats are [3,4,5], so return the lowest of them, which is 3.
-seatManager.Reserve();    // The available seats are [4,5], so return the lowest of them, which is 4.
-seatManager.Reserve();    // The only available seat is seat 5, so return 5.
-seatManager.Unreserve(5); // Unreserve seat 5, so now the available seats are [5].
-Console.WriteLine(  );
 public class Solution
 {
     #region 1. Two Sum
